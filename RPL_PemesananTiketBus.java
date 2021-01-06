8e9e1ce6a7b914001b56a50111467e0f5e9ce58c
Muhammad Raihan Suryanom <73580844+ReySuryanom@users.noreply.github.com>
2021-01-06 21:29:30 +0700

    Update RPL_PemesananTiketBus.java

--- RPL_PemesananTiketBus.java
+++ RPL_PemesananTiketBus.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 
 class PemesananTiketBus {
-	final int GET_HOURS = (new Random().nextInt(8-7) + 7);
-	final int GET_MINUTE = (new Random().nextInt(9-0) + 0);
 	private String name, id, date;
 	private int age, number, seatType;
 
@@ -18,10 +16,6 @@ class PemesananTiketBus {
 		this.date = date;
 		this.number = number;
 		this.seatType = seatType;
-	}
-
-	String getHours(int temp) {
-		return String.valueOf(GET_HOURS + ":" + temp + "" + GET_MINUTE);
 	}
 
 	String getName() {
@@ -48,13 +42,13 @@ class PemesananTiketBus {
 		return seatType;
 	}
 
-	protected void printInfo() {
+	protected void printInfo(int hour, int minute) {
 		int temp = (new Random().nextInt(5-1) + 1);
 		System.out.println("Cetak Tiket Pesanan . . . . .");
 		System.out.println(getSeatType() == 1 ? "Tiket pesanan untuk tempat duduk nomor (1-15)" : "Tiket pesanan untuk tempat duduk nomor (16-30)");
 		System.out.println("Nomor bus anda        : " + temp);
 		System.out.println("Nomor tempat duduk    : " + getNumber());
-		System.out.println("Jadwal keberangkatan  : 0" + getHours(temp));
+		System.out.println("Jadwal keberangkatan  : 0" + String.valueOf(hour + ":" + temp + "" + minute));
 		System.out.println("Tanggal pemesanan     : " + getDate());
 		System.out.println("Tanggal cetak pesanan : " + new java.text.SimpleDateFormat("dd/MM/yyyy | HH:mm:ss").format(new Date()));
 		System.out.println("Kode tiket            : " + getNumber() + getId() + String.valueOf(getName().charAt(0)).toUpperCase() + temp);
@@ -62,7 +56,13 @@ class PemesananTiketBus {
 }
 
 public class RPL_PemesananTiketBus {
+	final static Random random = new Random();
+	final static int hours = random.nextInt(8-7) + 7;
+	final static int minutes = random.nextInt(9-0) + 0;
+
 	public static void main(String[] args) {
+		final int hour = hours;
+		final int minute = minutes;
 		Scanner scan = new Scanner(System.in);
 		LinkedList<Integer> window = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
 		LinkedList<Integer> aisle = new LinkedList<>(Arrays.asList(16, 17, 18, 19, 20, 21, 23, 24, 25, 27, 28 ,29, 30));
@@ -115,7 +115,7 @@ public class RPL_PemesananTiketBus {
 
 		// Print ticket(s)
 		for (int i = 0; i < tickets.length; ++i)
-			tickets[i].printInfo();
+			tickets[i].printInfo(hour, minute);
 
 		System.out.println("\nTiket ini tidak bisa di refund!");
 		System.out.println("Patuhi peraturan dan hati-hati di jalan.\n");
